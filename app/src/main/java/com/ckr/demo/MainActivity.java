686db0091c734b218235884c2b1e80b0cf312810
ckr <chenckr@77ircloud.com>
2019-10-21 17:34:45 +0800

    Initial commit

---
+++ app/src/main/java/com/ckr/demo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ckr.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author ckr
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
